c9c1b6ca78803fe6a25fb19a43b2d13bf1266e60
csuzhang <csuzhang@tencent.com>
2022-07-10 23:19:24 +0800

    lint the code format

--- server/main.go
+++ server/main.go
@@ -1,20 +1,25 @@
+// Package main usage of singleflight.Group
 package main
 
 import (
 	"fmt"
 	"net/http"
 	"net/rpc"
-	"singleflight"
 	"sync"
 	"time"
+
+	"singleflight"
 )
 
+// Arg is the argument of Data.GetData
 type Arg struct {
 	Caller int
 }
 
+// Data is the RPC server struct
 type Data struct{}
 
+// GetData is the RPC server
 func (d *Data) GetData(arg *Arg, reply *string) error {
 	fmt.Printf("Request from client %d\n", arg.Caller)
 	time.Sleep(time.Second)
@@ -22,20 +27,25 @@ func (d *Data) GetData(arg *Arg, reply *string) error {
 	return nil
 }
 
+// main is the entry point of the program.
 func main() {
+	// create the RPC server
 	d := new(Data)
 	rpc.Register(d)
 	rpc.HandleHTTP()
 	fmt.Println("Starting RPC server...")
 	go call()
+	// start the RPC server
 	if err := http.ListenAndServe(":1234", nil); err != nil {
 		panic(err)
 	}
 }
 
+// call is the RPC client
 func call() {
+	// wait for the RPC server to start
 	time.Sleep(1 * time.Second)
-
+	// create the RPC client
 	ent, err := rpc.DialHTTP("tcp", "localhost:1234")
 	if err != nil {
 		panic(err)
@@ -43,7 +53,7 @@ func call() {
 	sf := new(singleflight.Group)
 	wg := sync.WaitGroup{}
 	wg.Add(100)
-
+	// 100 goroutines will call synced the RPC server
 	for i := 0; i < 100; i++ {
 		fn := func() (interface{}, error) {
 			var reply string
@@ -53,7 +63,7 @@ func call() {
 			}
 			return reply, nil
 		}
-
+		// call singleflight.Do in goroutine
 		go func(i int) {
 			result, _ := sf.Do("foo", fn)
 			fmt.Printf("caller: %d get result: %s\n", i, result)
